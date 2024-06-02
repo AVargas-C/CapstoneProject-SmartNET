e0882110dea70da8d23d89d75dbebc8e8be45a44
Arturo Vargas Cuevas <avargas2705@outlook.com>
2024-06-02 04:34:38 -0600

    reorganize

---
+++ tests/07_stadium_popup/stadium_popup.js
@@ -0,0 +1,49 @@
+/*-------------- POPUP -----------------*/
+function createPopup(id) {
+  let popupNode = document.querySelector(id);
+
+  let overlay = popupNode.querySelector(".overlay");
+  let closeBtn = popupNode.querySelector(".close-btn");
+
+  function openPopup() {
+    popupNode.classList.add("active");
+    overlay.classList.add("active");
+  }
+  function closePopup() {
+    popupNode.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+  overlay.addEventListener("click", closePopup);
+  closeBtn.addEventListener("click", closePopup);
+  return openPopup;
+}
+
+let popup = createPopup("#popup");
+
+document.querySelectorAll("svg g").forEach(function(element) {
+  element.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    popup(); // Open the popup
+  });
+});
+
+/*--------------   GAUGES CSS ------------*/
+const gaugeVoltage = document.querySelector("#voltage");
+const gaugeCurrent = document.querySelector("#current");
+
+function setGauge(gauge, value, max_value, reading_type) {
+if (value < 0 || value > max_value) {
+  return;
+}
+
+gauge.querySelector(".gauge__fill").style.transform = `rotate(${
+  value / (max_value * 2)
+}turn)`;
+gauge.querySelector(".gauge__cover").textContent = `${Math.round(
+  value * 1
+)} ${reading_type}`;
+}
+
+
+setGauge(gaugeVoltage, 100, 150, "V");
+setGauge(gaugeCurrent, 10, 20, "A");
